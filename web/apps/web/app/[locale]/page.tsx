d5353906fdc3d167c986e31d8eb960d13948c404
STARTRACEX <startraceebr@foxmail.com>
2023-10-07 22:45:51 +0800

    Add next-intl

---
+++ web/apps/web/app/[locale]/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { useTranslations } from "next-intl";
+
+export default function Page(): JSX.Element {
+  const t = useTranslations("Index");
+  return (
+    <>
+      <h1>{t("title")}</h1>;
+    </>
+  );
+}
