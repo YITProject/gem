c895ff1488192eb9e6b3840e7a5047c9832c475f
STARTRACEX <startraceebr@foxmail.com>
2023-10-09 20:38:08 +0800

    Warp jsonwebtoken method

---
+++ web/apps/web/common/jwt.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+const jwtkey = process.env.JWTKEY!;
+export const defaultSignOption: jwt.SignOptions = {
+  expiresIn: "24h",
+  algorithm: "HS512",
+};
+export function sign(
+  payload:
+    | string
+    | Buffer
+    | {
+        [key: string]: any;
+        password?: string | null | undefined;
+      },
+  options?: jwt.SignOptions,
+): string {
+  if (typeof payload == "object" && "password" in payload) {
+  }
+  return jwt.sign(payload, jwtkey, options || defaultSignOption);
+}
+
+export function parse(token: string): string | jwt.JwtPayload | null {
+  return jwt.verify(token, jwtkey, defaultSignOption);
+}
+export const verify: typeof parse = parse;
+
+export function decode(raw: string): string | jwt.JwtPayload | null {
+  return jwt.decode(raw);
+}
+
+export default {
+  sign,
+  verify,
+  parse,
+  decode,
+};
