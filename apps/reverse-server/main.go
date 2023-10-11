9b482fcaf74b8c1dc7c57ebd643725fcdeecd5fa
STARTRACEX <startraceebr@foxmail.com>
2023-10-12 00:29:20 +0800

    Add reverse example

--- apps/reverse-server/main.go
+++ apps/reverse-server/main.go
@@ -7,13 +7,43 @@ import (
 
 func main() {
 	r := reverse.New()
+	defer r.Run()
+
+	/*
+		Add these hosts in local
+		 * @see https://github.com/startracex/goup/reverse
+
+		127.0.0.1		example.com
+		127.0.0.1		login.example.com
+		127.0.0.1		docs.example.com
+
+	*/
+
 	r.Add(&reverse.Forward{
 		Form: &url.URL{
-			Host: "localhost",
+			Host: "example.com",
+		},
+		Target: &url.URL{
+			Host: "localhost:3000",
+		},
+	})
+
+	r.Add(&reverse.Forward{
+		Form: &url.URL{
+			Host: "docs.example.com",
+		},
+		Target: &url.URL{
+			Host: "localhost:3001",
+		},
+	})
+	
+	r.Add(&reverse.Forward{
+		Form: &url.URL{
+			Host: "login.example.com",
 		},
 		Target: &url.URL{
 			Host: "localhost:9527",
 		},
 	})
-	r.Run()
+
 }
