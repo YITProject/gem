5fb5450f20533db4f7ff7c280d3b922676923289
STARTRACEX <startraceebr@foxmail.com>
2023-10-12 23:36:42 +0800

    Update

--- apps/web/src/app/[locale]/_base-footer.tsx
+++ apps/web/src/app/[locale]/_base-footer.tsx
@@ -2,10 +2,10 @@ import { FlexFlow } from "godown/react";
 import Logo from "ui/logo/gemgames";
 import Link from "next-intl/link";
 import { css } from "powerstyl";
+
 export default function BaseFooter() {
-  
   return (
-    <footer slot="footer">
+    <footer slot="footer" style={css`display:contents;`}>
       <FlexFlow
         flexflow="column"
         style={css`
@@ -30,11 +30,11 @@ export default function BaseFooter() {
           >
             <Logo color="auto" height={42} />
             <Link
+              href="/"
               style={css`
                 margin: 6px;
                 font-size: 20px;
               `}
-              href={"/"}
             >
               Gem Games
             </Link>
@@ -42,18 +42,18 @@ export default function BaseFooter() {
 
           <div>
             <a
+              href="http://docs.example.com/ua"
               style={css`
                 margin: 6px;
               `}
-              href="http://docs.example.com/ua"
             >
               服务条款
             </a>
             <a
+              href="http://docs.example.com/pp"
               style={css`
                 margin: 6px;
               `}
-              href="http://docs.example.com/pp"
             >
               隐私政策
             </a>
@@ -61,16 +61,16 @@ export default function BaseFooter() {
         </section>
 
         <a
+          href="https://github.com/YITProject/gem"
           style={css`
             display: flex;
             justify-content: center;
             margin: 8px;
           `}
-          href="https://github.com/YITProject/gem"
         >
           &copy; {new Date().getFullYear()} GemGames
         </a>
-        <section></section>
+        <section />
       </FlexFlow>
     </footer>
   );
