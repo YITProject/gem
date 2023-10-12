b911067e9f5c0812dad1057b28a49b7a858893cd
STARTRACEX <startraceebr@foxmail.com>
2023-10-12 23:47:26 +0800

    Lint code

--- apps/web/src/state/user.ts
+++ apps/web/src/state/user.ts
@@ -2,20 +2,23 @@
 import { create } from "zustand";
 // import { persist } from "zustand/middleware";
 import { decode } from "jsonwebtoken";
-import { User } from "@prisma/client";
+import type { User } from "@prisma/client";
+
 export type UserDataSafeType = Partial<Omit<User, "password">>;
-const init = (set: any, get: any) => {
+const init = (set) => {
   return {
     data: null as UserDataSafeType | null,
-    loadFromJWT: (token?: string, autosave: boolean = true) => {
+    loadFromJWT: (token?: string, autosave = true) => {
       if (!token) {
-        token = localStorage.getItem("TOKEN") as string;
+        token = localStorage.getItem("TOKEN")!;
         if (!token) {
           window.location.href = `/login`;
+          return;
         }
       }
       const data = decode(token);
       if (data) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
         set(() => ({ data }));
       }
       if (autosave) {
