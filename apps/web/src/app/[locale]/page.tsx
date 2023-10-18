2eb27cc8540a21036197664bb42ce46e762a0d69
STARTRACEX <startraceebr@foxmail.com>
2023-10-18 11:31:49 +0800

    Mini index

--- apps/web/src/app/[locale]/page.tsx
+++ apps/web/src/app/[locale]/page.tsx
@@ -1,11 +1,7 @@
 "use client";
-import { useTranslations } from "next-intl";
-
 export default function Page(): JSX.Element {
-  const t = useTranslations("Index");
   return (
     <>
-      <h1>{t("title")}</h1>;
     </>
   );
 }
