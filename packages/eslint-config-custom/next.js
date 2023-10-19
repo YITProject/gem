fa2275d2bb779a84220e7e408c098fe527a58e9c
STARTRACEX <startraceebr@foxmail.com>
2023-10-19 15:35:08 +0800

    Add eslint rule

--- packages/eslint-config-custom/next.js
+++ packages/eslint-config-custom/next.js
@@ -41,6 +41,7 @@ module.exports = {
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/unbound-method": "off",
     "eslint-comments/require-description": "off",
     "import/no-default-export": "off",
     "import/no-extraneous-dependencies": "off",
